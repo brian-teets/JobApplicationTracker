2955af21c704260bb7859abb70c1405fcc43505a
Brian Teets <bwt17@msn.com>
2022-07-14 14:56:20 -0600

    a couple of additional inserts

--- DB/job_application.sql
+++ DB/job_application.sql
@@ -25,7 +25,8 @@ CREATE TABLE IF NOT EXISTS `jobapptrackerdb`.`job_application` (
   `job_title` VARCHAR(200) NOT NULL,
   `company_name` VARCHAR(500) NOT NULL,
   `link_to_description` TEXT NULL,
-  `posted_salary` DECIMAL(10,2) NULL,
+  `salary_low` DECIMAL(10,2) NULL,
+  `salary_high` DECIMAL(10,2) NULL,
   `location_work_model` VARCHAR(45) NULL,
   `contact_at_company` VARCHAR(100) NULL,
   `contact_notes` TEXT NULL,
@@ -56,7 +57,9 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `jobapptrackerdb`;
-INSERT INTO `jobapptrackerdb`.`job_application` (`id`, `job_title`, `company_name`, `link_to_description`, `posted_salary`, `location_work_model`, `contact_at_company`, `contact_notes`, `my_interest_level`, `app_status`, `offer_made`, `date_of_apply`, `date_of_offer`, `follow_up`, `follow_up_date`, `follow_up_note`) VALUES (1, 'Software Engineer', 'Spectrum', 'https://www.linkedin.com/jobs/view/3146217478', 76800.00, 'Hybrid', NULL, '', 7, 'Applied', False, NULL, NULL, NULL, NULL, '2022/07/13');
+INSERT INTO `jobapptrackerdb`.`job_application` (`id`, `job_title`, `company_name`, `link_to_description`, `salary_low`, `salary_high`, `location_work_model`, `contact_at_company`, `contact_notes`, `my_interest_level`, `app_status`, `offer_made`, `date_of_apply`, `date_of_offer`, `follow_up`, `follow_up_date`, `follow_up_note`) VALUES (1, 'Software Engineer', 'Spectrum', 'https://www.linkedin.com/jobs/view/3146217478', 76800.00, 135800, 'Hybrid', NULL, '', 7, 'Applied', false, '2022/07/13', NULL, NULL, NULL, '');
+INSERT INTO `jobapptrackerdb`.`job_application` (`id`, `job_title`, `company_name`, `link_to_description`, `salary_low`, `salary_high`, `location_work_model`, `contact_at_company`, `contact_notes`, `my_interest_level`, `app_status`, `offer_made`, `date_of_apply`, `date_of_offer`, `follow_up`, `follow_up_date`, `follow_up_note`) VALUES (2, 'Junior Engineer', 'DISH Network', 'https://www.linkedin.com/jobs/view/3161388088', 57400, 82000, NULL, NULL, NULL, 7, 'Applied', false, '2022/07/14', NULL, NULL, NULL, NULL);
+INSERT INTO `jobapptrackerdb`.`job_application` (`id`, `job_title`, `company_name`, `link_to_description`, `salary_low`, `salary_high`, `location_work_model`, `contact_at_company`, `contact_notes`, `my_interest_level`, `app_status`, `offer_made`, `date_of_apply`, `date_of_offer`, `follow_up`, `follow_up_date`, `follow_up_note`) VALUES (3, 'Full-Stack Developer (Python, SQL, HTML, CSS Web Frameworks, Javascript)', 'LUMEN', NULL, NULL, NULL, '100% Remote', NULL, NULL, 6, 'Applied', false, '2022/07/14', NULL, NULL, NULL, 'Recorded video interview questions and game challenges required within 5 days.');
 
 COMMIT;
 
